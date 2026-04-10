8dea68987ce62568574f91c3d067b81e8330a6c1
LisaHuelsmann <lisa.huelsmann@ur.de>
2026-04-10 12:21:47 +0200

    add first dataset

---
+++ R/data.R
@@ -0,0 +1,16 @@
+
+
+
+#' Weather data from Hurdal, Norway
+#'
+#' A dataset with weather data recorded in Hurdal, Norway.
+#'
+#' @format A data frame with 17520 rows and 4 variables:
+#' \describe{
+#'   \item{weather_Hurdal}{date, POSIXct}
+#'   \item{Precipitation (mm)}{Numeric observed value}
+#'   \item{Tair@42m (deg C)}{Numeric observed value}
+#'   \item{Tair@2m (deg C)}{Numeric observed value}
+#' }
+#' @source Holger Lange
+"weather_Hurdal"
