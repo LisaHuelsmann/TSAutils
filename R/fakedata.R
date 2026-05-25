ec4a08236e3abca2fcb54bef52c67a9d675b0f54
LisaHuelsmann <lisa.huelsmann@ur.de>
2026-05-25 17:17:37 +0200

    add fake data functions

--- R/fakedata.R
+++ R/fakedata.R
@@ -1,11 +1,20 @@
+
+#' Create Gaussian white noise
+#' @export
 purely.random <- function(n) rnorm(n)
 
+#' Add trend to a time series
+#' @export
 add.trend <- function(x, strength=1)
   x + seq(from=0, to=strength*sd(x), length=length(x))
 
+#' Add periodicity to a time series
+#' @export
 add.periodicity <- function(x, period=floor(length(x)/2))
   x + sapply(1:length(x), function(t) sin(2 * pi * t / period))
-  
+
+#' Add jumps to a time series
+#' @export
 add.jumps <- function(x, strength=1, rate=2/3*log(length(x))) {
   aha <- cumsum(floor(rexp(length(x), rate=rate))) + 1
   x + strength * (runif(max(aha)))[aha]
